d220882af495184172e8c838c80fdc81869c39af
Matthias Good <matthiasgood@gmail.com>
2018-05-10 15:15:16 +0200

    Add initial template structure

---
+++ src/main/kotlin/com/good/kotlinspringtemplate/KotlinSpringTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.good.kotlinspringtemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringTemplateApplication>(*args)
+}
